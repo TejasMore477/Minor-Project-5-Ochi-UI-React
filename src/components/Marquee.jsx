2e1460f1b9b79f4c0fa4d995aa81f546b338782e
Tejas More <tejasmore660@gmail.com>
2024-08-15 21:36:10 +0530

    used array to create same elem

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -2,13 +2,13 @@ import { motion } from 'framer-motion'
 import React from 'react'
 
 function Marquee() {
+ let marqueeElem = Array.from({length:4}).map((ele,index)=>(
+  <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear", repeat:Infinity, duration:10}} key={index} className='text-[20vw] font-bold font-title -mb-6'>We Are Ochi</motion.h1>
+ ))
   return (
     <div className='w-full py-12 bg-green-900 rounded-t-[3.4rem]'>
         <div className='h-fit border-b-2 border-t-2 border-gray-500  overflow-x-hidden overflow-y-hidden text-white flex whitespace-nowrap gap-10 leading-none'>
-            <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear", repeat:Infinity, duration:10}} className='text-[20vw] font-bold font-title -mb-6'>We Are Ochi</motion.h1>
-            <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear", repeat:Infinity, duration:10}} className='text-[20vw] font-bold font-title -mb-6'>We Are Ochi</motion.h1>
-            <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear", repeat:Infinity, duration:10}} className='text-[20vw] font-bold font-title -mb-6'>We Are Ochi</motion.h1>
-            <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear", repeat:Infinity, duration:10}} className='text-[20vw] font-bold font-title -mb-6'>We Are Ochi</motion.h1>
+          {marqueeElem}
         </div>
     </div>
   )
