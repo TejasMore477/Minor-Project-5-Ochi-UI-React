2172d7026978474f826c9dfe458a934415ca6330
Tejas More <tejasmore660@gmail.com>
2024-08-17 20:40:41 +0530

    changed color

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -6,7 +6,7 @@ function Marquee() {
   <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear", repeat:Infinity, duration:10}} key={index} className='md:text-[20vw] text-[30vw] font-bold font-title md:-mb-6 pr-10'>We Are Ochi</motion.h1>
  ))
   return (
-    <div   data-scroll data-scroll-section data-scroll-speed=".1" className='w-full md:py-12 py-6 bg-emerald-800 md:rounded-t-[3.4rem] rounded-[1rem] xl:bg-red-400 lg:bg-blue-400 md:bg-yellow-200 sm:bg-emerald-400'>
+    <div   data-scroll data-scroll-section data-scroll-speed=".1" className='w-full md:py-12 py-6 bg-emerald-800 md:rounded-t-[3.4rem] rounded-[1rem]'>
         <div className='h-fit border-b-2 border-t-2 border-gray-500  overflow-x-hidden overflow-y-hidden text-white flex whitespace-nowrap leading-none'>
           {marqueeElem}
         </div>
