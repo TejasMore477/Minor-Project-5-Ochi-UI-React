0d83fd72ae476344350479b31f7be9c53994a84e
Tejas More <tejasmore660@gmail.com>
2024-08-18 10:52:19 +0530

    eyes ui changes

--- src/components/StartProject.jsx
+++ src/components/StartProject.jsx
@@ -43,11 +43,11 @@ function StartProject() {
         <h1 className='font-extrabold md:text-[12.5vw] text-[18vw] uppercase md:leading-[10vw] leading-[17vw] text-center tracking-tighter'>to start</h1>
         <h1 className='font-extrabold md:text-[12.5vw] text-[18vw] uppercase md:leading-[10vw] leading-[17vw] text-center tracking-tighter'>the project?</h1>
 
-        <div  data-scroll data-scroll-section data-scroll-speed=".6" className='absolute md:top-[40%] top-[75%] md:left-[35%] left-[10%] md:-translate-x-[50%] -translate-y-[70%] flex items-center justify-center gap-5'>
+        <div  data-scroll data-scroll-section data-scroll-speed=".6" className='absolute md:top-[40%] top-[85%] md:left-[35%] left-[10%] md:-translate-x-[50%] -translate-y-[70%] flex items-center justify-center gap-5'>
           {elements}
         </div>
 
-        <div id='startProject' className='flex items-center justify-center gap-3 group mt-20'>
+        <div id='startProject' className='flex items-center justify-center gap-3 group mt-10'>
                 <button className='px-4 py-1  text-2xl rounded-full border-2 border-zinc-600 uppercase tracking-tight font-semibold group-hover:bg-black group-hover:text-white transition duration-700 '>start the project</button>
                 <div className='rounded-full border-2 border-zinc-600 p-2 text-2xl font-semibold group-hover:bg-black group-hover:text-white transition duration-700 '>
                     <MdArrowOutward />
